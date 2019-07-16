634faa1df31966a2253962b509b37cd9216dbeac
wangweitong <1097828409@qq.com>
2019-07-16 08:53:15 +0800

    文本分类

--- text_classification/text_classification.py
+++ text_classification/text_classification.py
@@ -4,3 +4,7 @@ from tensorflow import keras
 import numpy as np
 
 print(tf.__version__)
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
